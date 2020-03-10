5daecac4246fbc5230c191aa913aca17c73a2b31
Matt Gordon <bigmattsize21@gmail.com>
2020-03-10 21:47:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
   elsif array.length == 3
     "#{array[0]}, #{array[1]}, and #{array[2]}"
   elsif array.length > 3
-    "#{array.insert(0..1)}"
+    "#{array[0].insert(1)}"
     "#{array.join(", "," and")}"
 
   end
